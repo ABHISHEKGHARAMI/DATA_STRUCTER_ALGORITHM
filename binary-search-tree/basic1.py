ab61ddd29f32ea7ff974f35ca3745c175d7f0627
Abhishek Gharami <abhishekgharami1998@gmail.com>
2022-11-07 12:48:12 +0530

    adding

---
+++ binary-search-tree/basic1.py
@@ -0,0 +1,94 @@
+#basic in binary search tree
+class node:
+    def __init__(self,data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+def inOrder(root):
+    if root:
+        inOrder(root.left)
+        print(root.data)
+        inOrder(root.right)
+#insert data
+def insert(root,data):
+    if root == None:
+        return node(data)
+    else:
+        if root.data == data:
+            return root
+        elif root.data < data:
+            root.right = insert(root.right,data)
+        else:
+            root.left = insert(root.left,data)
+    return root
+
+#search value
+def search(root,data):
+    if root==None:
+        return False
+    else:
+        if root.data == data:
+            return True
+        elif root.data < data :
+            return search(root.right,data)
+        else:
+            return search(root.left,data)
+
+def min_value(root):
+    curr =root
+    while curr.left:
+        curr =  curr.left
+    return curr
+
+#delete key
+def delete(root,data):
+    if root is None:
+        return root
+    if root.data < data:
+        root.right = delete(root.right,data)
+    elif root.data > data:
+        root.left = delete(root.left,data)
+    else:
+        if root.left is None:
+            temp = root.right
+            root= None
+            return temp
+        elif root.right is None:
+            temp = root.left
+            root = None
+            return temp
+        else:
+            temp = min_value(root.right)
+            root.data = temp.data
+            root.right = delete(root.right,temp.data)
+    return root
+#levelorder traversal
+def  height(root):
+    if root==None:
+        return 0
+    else:
+
+        l = height(root.left)
+        r = height(root.right)
+
+        return max(l,r)+1
+def levelorder(root):
+    h = height(root)
+    for i in range(1,h+1):
+        level_utill(root,i)
+def level_utill(root,level):
+    if root==None:
+        return
+    if level == 1:
+        print(root.data,end=" ")
+    elif level>1:
+        level_utill(root.left,level-1)
+        level_utill(root.right,level-1)
+root = node(50)
+
+ar = [ 40,60,20,30,10,70]
+for  i in range(len(ar)):
+    insert(root,ar[i])
+inOrder(root)
+print("The min value of the tree is :",min_value(root).data)
---
+++ binary-search-tree/lowest_common_ancester1.py
@@ -0,0 +1,52 @@
+# lowest common ancester problem
+# it will return the least root node for the given node
+class node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+#l-c-a function
+
+
+def lca(root, n1, n2):
+    path1 = []
+    path2 = []
+    if not lca_utill_bforce(root, path1, n1) or not lca_utill_bforce(root, path2, n2):
+        return -1
+    i = 0
+    while i < len(path1) and i < len(path2):
+        if path1[i] != path2[i]:
+            break
+        i += 1
+    return path1[i-1]
+
+# lca utill brute force
+
+
+def lca_utill_bforce(root, path, n):
+    if root == None:
+        return False
+    path.append(root.data)
+    if root.data == n:
+        return True
+    if root.left != None and lca_utill_bforce(root.left, path, n) or root.right != None and lca_utill_bforce(root.right, path, n):
+        return True
+    path.pop()
+    return False
+
+
+#main
+root = node(1)
+root.left = node(2)
+root.right = node(3)
+root.left.left = node(4)
+root.left.right = node(5)
+root.right.left = node(6)
+root.right.right = node(7)
+
+print("LCA(4, 5) = %d" % (lca(root, 4, 5,)))
+print("LCA(4, 6) = %d" % (lca(root, 4, 6)))
+print("LCA(3, 4) = %d" % (lca(root, 3, 4)))
+print("LCA(2, 4) = %d" % (lca(root, 2, 4)))
+print("LCA(5, 6) = %d" % (lca(root, 5, 6)))
